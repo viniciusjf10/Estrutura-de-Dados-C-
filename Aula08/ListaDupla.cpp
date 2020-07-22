66c3045eea3aea187e20dda210ed8140feaf93fc
Vinicius Almeida <viniciusouzajf@gmail.com>
2020-07-21 23:07:15 -0300

    Exercicios aula 8 e 9

---
+++ Aula08/ListaDupla.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <cstdlib>
+#include "ListaDupla.h"
+
+using namespace std;
+
+ListaDupla::ListaDupla()
+{
+    cout << "Criando uma ListaDupla" << endl;
+    primeiro = NULL;
+    ultimo = NULL;
+    n = 0;
+}
+
+ListaDupla::~ListaDupla()
+{
+    cout << "Destruindo ListaDupla" << endl;
+    NoDuplo *p = primeiro;
+    while(p != NULL)
+    {
+        NoDuplo *t = p->getProx();
+        delete p;
+        p = t;
+    }
+}
+
+bool ListaDupla::busca(int val)
+{
+    NoDuplo *p;
+    for(p = primeiro; p != NULL; p = p->getProx())
+        if(p->getInfo() == val)
+            return true;
+    return false;
+}
+
+int ListaDupla::get(int k)
+{
+    NoDuplo *p = primeiro;
+    for(int i = 0; i < k; i++)
+    {
+        if(p == NULL)
+            break;
+        p = p->getProx();
+    }
+    if(p == NULL)
+    {
+        cout << "ERRO: Indice invalido!" << endl;
+        exit(1);
+    }
+    else
+        return p->getInfo();
+}
+
+void ListaDupla::set(int k, int val)
+{
+    NoDuplo *p = primeiro;
+    for(int i = 0; i < k; i++)
+    {
+        if(p == NULL)
+            break;
+        p = p->getProx();
+    }
+    if(p == NULL)
+        cout << "ERRO: Indice invalido!" << endl;
+    else
+        p->setInfo(val);
+}
+
+void ListaDupla::insereInicio(int val)
+{
+	NoDuplo *p = new NoDuplo();
+	p->setInfo(val);
+	p->setProx(primeiro);
+	p->setAnt(NULL);
+
+	if(n == 0) ultimo = p;
+	else primeiro->setAnt(p);
+
+	primeiro = p;
+	n = n + 1;
+}
+
+void ListaDupla::removeInicio()
+{
+	NoDuplo *p;
+	if(primeiro != NULL)
+	{
+		p = primeiro;
+		primeiro = p->getProx();
+		delete p;
+		n = n - 1;
+
+		if(n == 0) 	ultimo = NULL;
+		else primeiro->setAnt(NULL);
+	}
+	else
+        cout << "ERRO: lista vazia" << endl;
+}
+
+void ListaDupla::insereFinal(int val)
+{
+	NoDuplo *p = new NoDuplo();
+	p->setInfo(val);
+	p->setProx(NULL);
+	p->setAnt(ultimo);
+
+	if(n == 0) primeiro = p;
+	else ultimo->setProx(p);
+
+	ultimo = p;
+	n = n + 1;
+}
+
+void ListaDupla::removeFinal()
+{
+	NoDuplo *p;
+	if(primeiro != NULL)
+	{
+		p = ultimo;
+		ultimo = p->getAnt();
+		delete p;
+		n = n - 1;
+
+		if(n == 0) 	primeiro = NULL;
+		else ultimo->setProx(NULL);
+	}
+	else
+        cout << "ERRO: lista vazia" << endl;
+}
+
+void ListaDupla::imprime()
+{
+    NoDuplo *p;
+    p=primeiro;
+    for(int i = 0; p!=NULL; i++)
+    {
+        cout<<p->getInfo()<<", ";
+        p = p->getProx();
+    }
+    cout<<endl;
+}
+
+void ListaDupla::imprimeReverso()
+{
+    NoDuplo *p;
+    p=ultimo;
+    for(int i = 0; p!=NULL; i++)
+    {
+        cout<<p->getInfo()<<", ";
+        p = p->getAnt();
+    }
+    cout<<endl;
+}
+ListaDupla* ListaDupla::concatena(ListaDupla *l2)
+{
+    ListaDupla *nova = new ListaDupla();
+    for(NoDuplo *p = primeiro; p!= NULL; p = p->getProx())
+        nova->insereFinal(p->getInfo());
+    for(NoDuplo *p = l2->primeiro; p!=NULL; p=p->getProx())
+        nova ->insereFinal(p->getInfo());
+    return nova;
+
+}
